2c5b3d5073b7486ad10e34af21b76ac3597351f5
dcamposj <diegocj92@gmail.com>
2024-12-27 19:44:41 -0600

    react cdn setup complete

--- javascript/my-app.js
+++ javascript/my-app.js
@@ -1,2 +1,22 @@
-const element = React.createElement("h1", {}, "Hello React!");
-ReactDOM.render(element, document.getElementById("react-app"));
+function MyApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  const [count, setCount] = React.useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick}>Clicked {count} times</button>;
+}
+
+// Renderiza la aplicación
+ReactDOM.render(<MyApp />, document.getElementById("root"));
